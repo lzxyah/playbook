cdf06c798505c648691246693762c825a8dc2de0
lzxyah <lzx000@vip.qq.com>
2018-06-09 19:35:38 +0800

    Update scp1.sh

--- scp1.sh
+++ scp1.sh
@@ -2,7 +2,7 @@
 set timeout 5
 spawn scp -3  172.16.18.31:/root/mysql_dump.sql 172.16.18.32:/root/
 expect "root@172.16.18.31's password: "
-send wRG2EM%F!pG3XpgZnwcz\n
+send 9999999999\n
 expect "root@172.16.18.32's password: "
-send wRG2EM%F!pG3XpgZnwcz\n
+send ppppppp\n
 interact
